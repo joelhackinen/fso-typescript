993a76a15aaf2258b26dbf29159fb417e369667a
joel_laptop <joel.hakkinengmail.com>
2023-03-11 11:17:25 +0200

    9.10

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 
+import data from './data/diagnoses';
+import { Diagnose } from './types';
+
 const app = express();
 
 app.use(cors());
@@ -12,6 +15,11 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.get('/api/diagnoses', (_req, res) => {
+  const diagnoses: Diagnose[] = data;
+  res.json(diagnoses);
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
